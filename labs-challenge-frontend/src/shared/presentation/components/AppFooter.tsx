74b894cb38a8c21e1a4e1f082a8972d4f6268451
gianhp12 <gian_htc@hotmail.com>
2025-06-21 17:57:26 -0300

    Fix: Add flutter project

--- labs-challenge-frontend/src/shared/presentation/components/AppFooter.tsx
+++
@@ -1,7 +0,0 @@
-export function AppFooter() {
-  return (
-    <footer className="mt-auto bg-gray-100 py-4 text-center text-sm text-gray-600 shadow-inner">
-      &copy; {new Date().getFullYear()} Labs Challenge. Todos os direitos reservados.
-    </footer>
-  );
-}
